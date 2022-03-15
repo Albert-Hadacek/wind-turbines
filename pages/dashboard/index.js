5bec9aa93985b44aa576545ce367f0cb60671c29
Pawel Zmarlak <zmarlakpawel@gmail.com>
2022-03-15 14:25:08 +0000

    No buttons on dashboard, just simple anchors

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
-import ButtonEmphasis from '../../components/button/button'
+import React, { Component } from "react";
+import ButtonEmphasis from "../../components/button/button";
+import Link from "next/link";
 
 export default function Dashboard() {
-    return (
-      <div>
-        <h1>Dashboard</h1>
-        <ButtonEmphasis/>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <h2>
+        <Link href="/">
+          <a>View all wind turbines</a>
+        </Link>
+        {' '}
+        <Link href="/">
+          <a>Add new</a>
+        </Link>
+      </h2>
+    </div>
+  );
+}
